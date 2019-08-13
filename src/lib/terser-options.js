411fc82934b62deb89bb18e0a3c116cbad710236
rogeriopvl <rogeriopvl@gmail.com>
2019-08-13 17:16:29 +0100

    feat: add all terser default options as module

---
+++ src/lib/terser-options.js
@@ -0,0 +1,118 @@
+const options = {
+  parse: {
+    bare_returns: false,
+    ecma: 8,
+    html5_comments: true,
+    shebang: true
+  },
+
+  compress: {
+    arrows: true,
+    arguments: false,
+    booleans: true,
+    boolean_as_integers: false,
+    collapse_vars: true,
+    comparisons: true,
+    computed_props: true,
+    conditionals: true,
+    dead_code: true,
+    defaults: true,
+    directives: true,
+    drop_console: false,
+    drop_debugger: true,
+    ecma: 5,
+    evaluate: true,
+    expression: false,
+    global_defs: {},
+    hoist_funs: false,
+    hoist_props: true,
+    hoist_vars: false,
+    if_return: true,
+    inline: true,
+    join_vars: true,
+    keep_classnames: false,
+    keep_fargs: true,
+    keep_fnames: false,
+    keep_infinity: false,
+    loops: true,
+    module: false,
+    negate_iife: true,
+    passes: 1,
+    properties: true,
+    pure_funcs: null,
+    pure_getters: 'strict',
+    reduce_funcs: true,
+    reduce_vars: true,
+    sequences: true,
+    side_effects: true,
+    switches: true,
+    toplevel: false,
+    top_retain: null,
+    typeofs: true,
+    unsafe: false,
+    unsafe_arrows: false,
+    unsafe_comps: false,
+    unsafe_Function: false,
+    unsafe_math: false,
+    unsafe_methods: false,
+    unsafe_proto: false,
+    unsafe_regexp: false,
+    unsafe_undefined: false,
+    unused: true,
+    warnings: false
+  },
+
+  mangle: {
+    eval: false,
+    keep_classnames: false,
+    keep_fnames: false,
+    module: false,
+    reserved: [],
+    toplevel: false,
+    safari10: false,
+    properties: {
+      builtins: false,
+      debug: false,
+      keep_quoted: false,
+      regex: null,
+      reserved: []
+    }
+  },
+
+  output: {
+    ascii_only: false,
+    beautify: true,
+    braces: false,
+    comments: false,
+    ecma: 5,
+    indent_level: 4,
+    indent_start: 0,
+    inline_script: true,
+    keep_quoted_props: false,
+    max_line_len: false,
+    preamble: null,
+    quote_keys: false,
+    quote_style: 0,
+    safari10: false,
+    semicolons: true,
+    shebang: true,
+    webkit: false,
+    wrap_iife: false
+  },
+
+  sourceMap: {},
+
+  ecma: 5,
+
+  keep_classnames: false,
+
+  keep_fnames: false,
+  ie8: false,
+  module: false,
+  nameCache: null,
+  safari10: false,
+  toplevel: false,
+  warnings: false
+};
+
+export default options;
