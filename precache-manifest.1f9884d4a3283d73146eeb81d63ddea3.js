4d9a85b4b2559ab29a44f8c8eaeeae0d93b583f0
Fábio Santos <fabiosantosart@gmail.com>
2020-04-18 18:28:57 +0100

    Updates

---
+++ precache-manifest.1f9884d4a3283d73146eeb81d63ddea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e4debde65131de8f4cbb30ebe58fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f129e0e8ccf427386069",
+    "url": "/static/css/2.cbb012f8.chunk.css"
+  },
+  {
+    "revision": "353267c58136514bc684",
+    "url": "/static/css/main.e6aed224.chunk.css"
+  },
+  {
+    "revision": "f129e0e8ccf427386069",
+    "url": "/static/js/2.b50ba7e1.chunk.js"
+  },
+  {
+    "revision": "2a7ddd6924cd19423573c8c25a45983c",
+    "url": "/static/js/2.b50ba7e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353267c58136514bc684",
+    "url": "/static/js/main.4c3edd61.chunk.js"
+  },
+  {
+    "revision": "3d0e831631df92c636d3",
+    "url": "/static/js/runtime-main.684f5b3a.js"
+  }
+]);
