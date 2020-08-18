b67f5439fcf4b220a0605eac49b786fde61070c3
Fábio Santos <fabiosantosart@gmail.com>
2020-08-18 16:11:20 +0100

    Updates

--- precache-manifest.6bdcc6b8a19ef84d315a452c0e8ba583.js
+++ precache-manifest.6bdcc6b8a19ef84d315a452c0e8ba583.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ad37ccffdd4ceb905a357c828a447a1",
+    "revision": "f1e48e769fd33cd0ff3c5ddc7e9775b0",
     "url": "/index.html"
   },
   {
-    "revision": "f129e0e8ccf427386069",
+    "revision": "5ac2c2a1a3c3e6ddcb95",
     "url": "/static/css/2.cbb012f8.chunk.css"
   },
   {
-    "revision": "e6bace984d2d08bd86ef",
+    "revision": "87dd1c4e69818285ca66",
     "url": "/static/css/main.8e458a2a.chunk.css"
   },
   {
-    "revision": "f129e0e8ccf427386069",
-    "url": "/static/js/2.b50ba7e1.chunk.js"
+    "revision": "5ac2c2a1a3c3e6ddcb95",
+    "url": "/static/js/2.8f30af45.chunk.js"
   },
   {
     "revision": "2a7ddd6924cd19423573c8c25a45983c",
-    "url": "/static/js/2.b50ba7e1.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8f30af45.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e6bace984d2d08bd86ef",
-    "url": "/static/js/main.4c3edd61.chunk.js"
+    "revision": "87dd1c4e69818285ca66",
+    "url": "/static/js/main.fb5a279f.chunk.js"
   },
   {
     "revision": "3d0e831631df92c636d3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4192827f992b8c1cd490c34a00b664d3.js"
+  "/precache-manifest.6bdcc6b8a19ef84d315a452c0e8ba583.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
