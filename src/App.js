56f4405ee2dc40adb6dfcae76e20d33830617190
Fábio Santos <fabiosantosart@gmail.com>
2020-11-15 13:25:01 +0000

    add source-map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import loadScript from 'load-script'
 
 import Repl from './Repl'
 import Header from './Header';
@@ -8,49 +7,27 @@ import styles from './App.module.css'
 
 export default class App extends React.Component {
   state = {
-    loading: true,
     error: null,
-    terser: null
   }
   componentDidMount() {
-    const scriptPromise = new Promise((resolve, reject) => {
-      loadScript('https://cdn.jsdelivr.net/npm/terser/dist/bundle.min.js', (err) => {
-        if (err) return reject(err)
-        if (!window.Terser) return reject(new Error('Could not load Terser from jsdelivr'))
-        resolve(window.Terser)
+    if (!window.Terser) {
+      this.setState({
+        error: new Error('Could not load Terser from jsdelivr')
       })
-    })
-
-    scriptPromise
-      .then(terser => {
-        this.setState({
-          loading: false,
-          error: null,
-          terser
-        })
-      })
-      .catch(error => {
-        this.setState({
-          loading: false,
-          error,
-          terser: null
-        })
-      })
+    }
   }
   render() {
-    const { loading, error, terser } = this.state
+    const { error } = this.state
 
     const body = (() => {
-      if (loading) {
-        return <div className={styles.message}>Loading...</div>
-      } else if (error) {
+      if (error) {
         return (
           <div className={styles.message + ' ' + styles.error}>
             {error?.message || 'An error has occurred'}
           </div>
         )
       } else {
-        return <Repl terser={terser} />
+        return <Repl terser={window.Terser} />
       }
     })()
 
