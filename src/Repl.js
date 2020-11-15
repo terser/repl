504e00600f22bf446548b0dcb4774c80e56aaa40
Fábio Santos <fabiosantosart@gmail.com>
2020-11-15 13:35:35 +0000

    update packages

--- src/Repl.js
+++ src/Repl.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import debounce from 'lodash.debounce';
-import cloneDeep from 'lodash.clonedeep';
+import { debounce, cloneDeep } from 'lodash-es';
 
 import CodeMirrorPanel from './CodeMirrorPanel';
 import { getCodeSizeInBytes } from './lib/helpers';
