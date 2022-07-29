95456ba42ef3670920fb634cb2cfe566016c7778
SukkaW <isukkaw@gmail.com>
2022-07-30 01:56:42 +0800

    Add corresponding style to share button

--- src/Header.js
+++ src/Header.js
@@ -24,15 +24,17 @@ const Header = ({ shareUrl = '' }) => {
   return (
     <div className={styles.container}>
       <header>
-        <a href="https://terser.org">
-          <img
-            src="https://terser.org/img/terser-square-logo.png"
-            alt=""
-            role="presentation"
-          />
-          <h1>Terser REPL</h1>
-        </a>
-        <p>Try Terser on this page</p>
+        <div>
+          <a href="https://terser.org">
+            <img
+              src="https://terser.org/img/terser-square-logo.png"
+              alt=""
+              role="presentation"
+            />
+            <h1>Terser REPL</h1>
+          </a>
+          <p>Try Terser on this page</p>
+        </div>
         <button onClick={handleShareButtonClick}>{buttonText}</button>
       </header>
     </div>
