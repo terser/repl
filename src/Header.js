832f4608cd9d8484e0700e0be325908353a2c594
SukkaW <isukkaw@gmail.com>
2022-07-30 01:46:18 +0800

    Feat: bring up share function

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 import styles from './Header.module.css';
 
-const Header = () => {
+const Header = ({ shareUrl = '' }) => {
+  const [buttonText, setButtonText] = useState('Share');
+
+  const handleShareButtonClick = useCallback(async () => {
+    // update addressbar with shareUrl
+    window.history.replaceState(null, '', shareUrl)
+
+    if (!navigator.clipboard) {
+      prompt('Clipboard API is not supported in your environment. Please manually copy the following URL', shareUrl);
+    } else {
+      await navigator.clipboard.writeText(shareUrl)
+      setButtonText('URL is copied to clipboard!')
+
+      setTimeout(() => {
+        setButtonText('Share')
+      }, 3000)
+    }
+  }, [shareUrl]);
+
   return (
     <div className={styles.container}>
       <header>
@@ -15,6 +33,7 @@ const Header = () => {
           <h1>Terser REPL</h1>
         </a>
         <p>Try Terser on this page</p>
+        <button onClick={handleShareButtonClick}>{buttonText}</button>
       </header>
     </div>
   );
--- src/Repl.js
+++ src/Repl.js
@@ -5,6 +5,9 @@ import CodeMirrorPanel from './CodeMirrorPanel';
 import { getCodeSizeInBytes } from './lib/helpers';
 import terserOptions, { evalOptions } from './lib/terser-options';
 
+import { gzip, ungzip } from 'pako'
+import { Base64 } from 'js-base64'
+
 import styles from './Repl.module.css';
 
 const DEBOUNCE_DELAY = 500;
@@ -64,18 +67,47 @@ class Repl extends Component {
     );
   }
 
+  componentDidMount() {
+    const url = new URL(window.location.href)
+    const encodedInput = url.searchParams.get('code')
+    const encodedOptionsCode = url.searchParams.get('options')
+
+    if (encodedOptionsCode) {
+      const decodedOptionsCode = evalOptions(ungzip(Base64.toUint8Array(encodedOptionsCode), { to: 'string' }));
+      this.setState({
+        optionsCode: decodedOptionsCode
+      })
+      this._updateTerserOptions(decodedOptionsCode);
+    }
+
+    if (encodedInput) {
+      const decodedCode = ungzip(Base64.toUint8Array(encodedInput), { to: 'string' });
+      this._updateCode(decodedCode);
+      this._minify();
+    }
+  }
+
+  _getShareUrl = (code, optionsCode) => {
+    const url = new URL(window.location.href)
+    const encodedInput = Base64.fromUint8Array(gzip(code))
+    url.searchParams.set('code', encodedInput)
+    const encodedConfig = Base64.fromUint8Array(gzip(JSON.stringify(optionsCode)))
+    url.searchParams.set('options', encodedConfig)
+    return url.toString()
+  }
+
   _updateCode = code => {
     this.setState({
       code,
       rawSize: getCodeSizeInBytes(code)
     });
     this._minifyToState(code);
+    this.props.onReplShareUrlChange(this._getShareUrl(code, this.state.optionsCode))
   };
 
   _updateTerserOptions = options => {
     try {
       const parsedOptions = evalOptions(options);
-
       this.setState({
         terserOptions: parsedOptions,
         optionsErrorMessage: null
@@ -84,6 +116,7 @@ class Repl extends Component {
       this.setState({ optionsErrorMessage: e.message });
     }
 
+    this.props.onReplShareUrlChange(this._getShareUrl(this.state.code, options))
     this._minify(this.state.code);
   };
 
@@ -110,6 +143,8 @@ class Repl extends Component {
         });
       }
     } catch (e) {
+      console.error(e);
+      console.log(terserOpts);
       this.setState({ errorMessage: e.message });
     }
   };
